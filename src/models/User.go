109a223f1ea2fe7e8d61c7346c398d5e4ec7322f
jmbessa <jm.bessa09@gmail.com>
2024-02-28 00:32:21 -0300

    Initialization of environment variables and creation of the user model

---
+++ src/models/User.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+//User represents a user using the social media
+type User struct {
+	ID        uint      `json:"id, omitempty"`
+	Name      string    `json:"name, omitempty"`
+	Nick      string    `json:"nick, omitempty"`
+	Email     string    `json:"email, omitempty"`
+	Password  string    `json:"password, omitempty"`
+	CreatedAt time.Time `json:"CreatedAt, omitempty"`
+}
